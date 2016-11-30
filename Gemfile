b47fde8136c44fab3e3ba6528b444ed4c973033a
Dan Reich <dancreich@gmail.com>
2016-11-30 14:09:28 -0500

    Link.hottest_first sorts links by score.

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ end
 
 group :test do
   gem 'database_cleaner'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+RSpec.describe Link, ".hottest_first" do
+  it "returns the links: hottest to coldest" do
+    coldest_link = create(:link, upvotes: 3, downvotes: 3)
+    hottest_link = create(:link, upvotes: 5, downvotes: 1)
+    lukewarm_link = create(:link, upvotes: 2, downvotes: 1)
+
+    expect(Link.hottest_first).to eq [hottest_link, lukewarm_link, coldest_link]
+  end
+end
 
 RSpec.describe Link, "#upvote" do
   it "increments upvotes" do
